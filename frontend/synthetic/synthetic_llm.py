f05a1180b8778a7660f1de0c1f4d4e5553283bf6
nacho.corcuera <nacho.corcuera@veeva.com>
2024-09-25 18:31:03 +0200

    correct synthetic data generation to be correct

--- frontend/synthetic/synthetic_llm.py
+++ frontend/synthetic/synthetic_llm.py
@@ -2,24 +2,22 @@ from typing import List
 
 from pydantic import BaseModel
 
-from commons.constants import DEFAULT_LLM_MODEL, SYNTHETHIC_PROMPT, SYSTEM_MESSAGE_SYNTHETIC
-from commons.enum import LLMFactory
+from commons.config import get_environment_variables
+from commons.constants import SYNTHETHIC_PROMPT, SYSTEM_MESSAGE_SYNTHETIC
 from commons.llm.factory import LLMProviderFactory
 from commons.prompts.prompt_template import PromptTemplate
 
+env = get_environment_variables()
 
 class DataSyntheticLLM:
 
     def __init__(self,
                  model: BaseModel,
-                 llm_model: str =DEFAULT_LLM_MODEL,
-                 llm_provider: str = LLMFactory.openai.value,
                  prompt_path: str = SYNTHETHIC_PROMPT,
 
                  ):
 
-        self._llm_model = llm_model
-        self._llm_runner = LLMProviderFactory.get_provider(llm_provider)(model=List[model],
+        self._llm_runner = LLMProviderFactory.get_provider(env.LLM_PROVIDER_MODEL)(model=List[model],
                                                                          system_message=SYSTEM_MESSAGE_SYNTHETIC)
         self._prompt = PromptTemplate(prompt_path)
 
